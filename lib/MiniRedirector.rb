07e11e6e642bd31c1fe5875281c085c3b8606193
Matthias Frick <matthias@frick-web.at>
2011-05-03 11:54:20 +0200

    added first steps for redirector

--- lib/MiniRedirector.rb
+++ lib/MiniRedirector.rb
@@ -1,3 +1,14 @@
-module Miniredirector
-  # Your code goes here...
+module MiniRedirector
+  class MyMiddleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      headers = Hash.new
+      headers["Location"] = "http://www.railscasts.com"
+      status = 301
+      [ status, headers, "You are redirected to railscasts.." ]
+    end
+  end
 end
